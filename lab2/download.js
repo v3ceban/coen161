31a97d4ffef3020697daa4ee74919ba802d6d395
Vladimir Ceban <v3ceban@proton.me>
2024-01-25 18:10:31 -0800

    lab2 finished

--- lab2/download.js
+++ lab2/download.js
@@ -1,22 +1,15 @@
 //eslint-disable-next-line
 function downloadPage() {
-  // Get the current URL
   var url = window.location.href;
   const fileName = document.querySelector("h1");
-  console.log(fileName);
 
-  // Create a new XMLHttpRequest object
   var xhr = new XMLHttpRequest();
 
-  // Set the response type to 'text'
   xhr.responseType = "text";
 
-  // Define a callback function to handle the response
   xhr.onload = function() {
     if (xhr.status === 200) {
-      // The request was successful
       var content = xhr.responseText;
-      // start downloading content into a text file
       var element = document.createElement("a");
       element.setAttribute(
         "href",
@@ -28,14 +21,11 @@ function downloadPage() {
       element.click();
       document.body.removeChild(element);
     } else {
-      // The request failed
       console.error("Request failed. Status code: " + xhr.status);
     }
   };
 
-  // Open a GET request to the current URL
   xhr.open("GET", url);
 
-  // Send the request
   xhr.send();
 }

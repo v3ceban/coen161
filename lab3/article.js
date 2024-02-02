bdd1b8c95747a2fa166106b6c94f4eff10a91617
Vladimir Ceban <v3ceban@proton.me>
2024-02-01 17:28:54 -0800

    lab3 part 2

--- lab3/article.js
+++ lab3/article.js
@@ -23,34 +23,27 @@ window.addEventListener("scroll", () => {
   saveScrollPosition();
 });
 
-// Attach an event handler to the mouseup or touchup event of the article.
 document.addEventListener("mouseup", handleHighlight);
 document.addEventListener("touchend", handleHighlight);
 
-// Get the document's current selection and get the range starting at 0.
 function handleHighlight() {
   let selection = window.getSelection();
   let range = selection.getRangeAt(0);
 
-  // Create a new highlight node that surrounds the highlighted element.
   let highlightNode = document.createElement("span");
   highlightNode.classList.add("highlight");
   range.surroundContents(highlightNode);
 
-  // When a highlighted element is clicked, it should stop being highlighted
   highlightNode.addEventListener("click", removeHighlight);
 }
 
-// Remove the highlight class from the clicked element
 function removeHighlight(event) {
   event.target.classList.remove("highlight");
 }
 
-// Attach an event handler to the download button's click event.
 let downloadButton = document.getElementById("downloadButton");
 downloadButton.addEventListener("click", downloadHighlightedText);
 
-// Loop through all the elements that have been highlighted and save the element’s string text into a new array
 function downloadHighlightedText() {
   let highlightedElements = document.getElementsByClassName("highlight");
   let highlightedTextArray = [];
@@ -60,11 +53,9 @@ function downloadHighlightedText() {
     highlightedTextArray.push(highlightedText);
   }
 
-  // Convert the new array to a JSON string and call encodeURIComponent
   let jsonString = JSON.stringify(highlightedTextArray);
   let encodedString = encodeURIComponent(jsonString);
 
-  // Create an anchor element that can download this string
   let downloadLink = document.createElement("a");
   downloadLink.href = "data:text/plain;charset=utf-8," + encodedString;
   downloadLink.download = "highlightedText.json";
